fa4bf2736385b5caa6ea830ed57287a624e487d1
이지민 <jmlee@emro.co.kr>
2025-05-08 15:00:16 +0900

    edit navItems

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,17 +1,19 @@
-import { getBlogPosts } from 'app/blog/utils'
+import { getBlogPosts } from "app/blog/utils";
+import { MetadataRoute } from "next";
 
-export const baseUrl = 'https://portfolio-blog-starter.vercel.app'
+export const baseUrl = "https://portfolio-blog-starter.vercel.app";
 
-export default async function sitemap() {
+// 크롤러 봇이 사이트를 크롤링할 때, robots.txt 파일을 통해 크롤링하는 페이지 정보를 알 수 있다.
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   let blogs = getBlogPosts().map((post) => ({
     url: `${baseUrl}/blog/${post.slug}`,
     lastModified: post.metadata.publishedAt,
-  }))
+  }));
 
-  let routes = ['', '/blog'].map((route) => ({
+  let routes = ["", "/blog"].map((route) => ({
     url: `${baseUrl}${route}`,
-    lastModified: new Date().toISOString().split('T')[0],
-  }))
+    lastModified: new Date().toISOString().split("T")[0],
+  }));
 
-  return [...routes, ...blogs]
+  return [...routes, ...blogs];
 }
