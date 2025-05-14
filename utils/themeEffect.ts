5393117b8706ceae2d772658f909ba0b0de30cdd
이지민 <jmlee@emro.co.kr>
2025-05-14 17:47:58 +0900

    feat : themeEffect

---
+++ utils/themeEffect.ts
@@ -0,0 +1,3 @@
+export const themeEffect = () => {
+  document.documentElement.classList.toggle("dark", localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches));
+};
