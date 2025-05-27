80e7ab04aec400cb3435cf445345aac567a5e810
masibasi <jimin_lee98@naver.com>
2025-05-27 18:09:11 +0900

    fix : 날짜 컬럼 너비 변경

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { BlogPosts } from "app/components/posts";
 export default function Page() {
   return (
     <section className="flex flex-col items-center">
-      <div className="w-full bg-yellow-200 text-yellow-900 text-center py-2 text-sm font-semibold mb-4 rounded">🚧 This blog is under construction! Some features may change or be incomplete. 🚧</div>
+      {/* <div className="w-full bg-yellow-200 text-yellow-900 text-center py-2 text-sm font-semibold mb-4 rounded">🚧 This blog is under construction! Some features may change or be incomplete. 🚧</div> */}
       <div className="flex flex-row items-center w-full max-w-2xl mb-4 justify-between">
         <h1 className="text-2xl font-semibold tracking-tighter">Jimin's Dev Blog</h1>
       </div>
