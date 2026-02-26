e8ac45e87b3155e0b81c2c5c01ae9bdcdcfc1e9a
Jimin Lee <leejimin@usc.edu>
2026-02-25 16:13:21 -0800

    style : cardinal color on post list dates

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -20,7 +20,7 @@ export function Posts({ lang }: PostsProps = {}) {
         .map((post) => (
           <Link key={post.slug} className="flex flex-col space-y-1 mb-4 group" href={`/${lang && lang !== "en" ? lang + "/" : ""}posts/${post.slug}`}>
             <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <p className="text-neutral-500 dark:text-neutral-400 w-[120px] tabular-nums">{formatDate(post.metadata.publishedAt, false)}</p>
+              <p className="text-cardinal-600 dark:text-cardinal-500 w-[120px] tabular-nums">{formatDate(post.metadata.publishedAt, false)}</p>
               <p className="text-neutral-900 dark:text-neutral-100 tracking-tight flex-1 group-hover:text-cardinal-700 dark:group-hover:text-cardinal-400 transition-colors">{post.metadata.title}</p>
             </div>
           </Link>
